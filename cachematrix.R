4ed7b19f7a1ebdb1794593a1cf2b6bb137deabaf
Vasya Redneck <vasya.redneck@gmail.com>
2015-04-15 17:06:00 +0300

    The very first implementatio of makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## In this example we introduce the <<- operator which can be used to assign a value to an object
+## in an environment that is different from the current environment.
+## Below are two functions that are used to create a special object that stores a numeric matrix
+## and cache's its inverse.
 
-## Write a short comment describing this function
-
+## makeCacheMatrix function creates a special "matrix", which is really a list containing a function to
+## get the value of the matrix
+## set the value of the matrix
+## get the value of the inverse matrix (lazy)
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    get <- function() x
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL ## reset the cache
+    }
+    inverse <- function(...) {
+        if (is.null(inv)) {
+            message("Caching the inverse matrix")
+            inv <<- solve(x, ...)
+        }
+        message("Getting cached inverse matrix")
+        inv
+    }
+    list(set = set, get = get, inverse = inverse)
 }
 
 
-## Write a short comment describing this function
-
+## I have made this task in object oriented style. So cacheSolve function is
+## not really needed. Because it just delegates the call to the inverse method of
+## cacheMatrix object
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    x$inverse(...)
 }
